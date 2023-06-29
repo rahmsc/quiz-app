3571be9aac2fbac305cd1090837829da69627586
Daniel Milad <dan@Daniels-MacBook-Air.local>
2023-06-29 08:37:37 +0800

    first commit

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,28 @@
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🔥 🏆";
+  if (percentage >= 80 && percentage < 100) emoji = "🥇";
+  if (percentage >= 50 && percentage < 80) emoji = "🥈";
+  if (percentage >= 30 && percentage < 50) emoji = "🥉";
+  if (percentage < 30) emoji = "💩";
+
+  return (
+    <>
+      <p className="result">
+        {emoji} You scored <strong>{points} </strong> out of {maxPossiblePoints}{" "}
+        ({Math.ceil(percentage)}%) {emoji}
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn ui"
+        onClick={() => dispatch({ type: "reset" })}
+      >
+        Restart Quiz
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
